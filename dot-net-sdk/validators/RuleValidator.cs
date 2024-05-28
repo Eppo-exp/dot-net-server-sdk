c22ae147dd9decd79a0493d6dda1c24730d06898
Ty Potter <tyler@fluxon.com>
2024-05-27 23:26:52 -0600

    Rule evaluation

--- dot-net-sdk/validators/RuleValidator.cs
+++ dot-net-sdk/validators/RuleValidator.cs
@@ -5,108 +5,129 @@ using NuGet.Versioning;
 
 namespace eppo_sdk.validators;
 
-public class RuleValidator
+
+
+public static class RuleValidator
 {
-    public static Rule? FindMatchingRule(SubjectAttributes subjectAttributes, List<Rule> rules)
-    {
-        return rules.Find(rule => MatchesRule(subjectAttributes, rule));
-    }
+    // public static FlagEvaluation? EvaluateFlag(Flag flag, string subjectKey, Dictionary<string, object> subjectAttributes)
+    // {
+    //     if (!flag.Enabled) return null;
 
-    private static bool MatchesRule(SubjectAttributes subjectAttributes, Rule rule)
-    {
-        List<bool> conditionEvaluations = EvaluateRuleCondition(subjectAttributes, rule.conditions);
-        return !conditionEvaluations.Contains(false);
-    }
+    //     var now = DateTime.UtcNow.ToUnixTimeSeconds();
+    //     foreach (var allocation in flag.Allocations)
+    //     {
+    //         if (allocation.StartAt.HasValue && allocation.StartAt.Value > now)
+    //         {
+    //             continue;
+    //         }
+    //         if (allocation.EndAt.HasValue && allocation.EndAt.Value < now)
+    //         {
+    //             continue;
+    //         }
 
-    private static List<bool> EvaluateRuleCondition(SubjectAttributes subjectAttributes, List<Condition> ruleConditions)
-    {
-        return
-            ruleConditions.ConvertAll(condition => EvaluateCondition(subjectAttributes, condition));
-    }
+    //         var subject = new Dictionary<string, object>() { { "id", subjectKey } };
+    //         subject.Concat(subjectAttributes);
+    //         if (MatchesAnyRule(allocation.Rules, subject))
+    //         {
+    //             foreach (var split in allocation.Splits)
+    //             {
+    //                 if (MatchesAllShards(split.Shards, subjectKey, flag.TotalShards))
+    //                 {
+    //                     return new FlagEvaluation(flag.Variations[split.VariationKey], allocation.DoLog, allocation.Key);
+    //                 }
+    //             }
+    //         }
+    //     }
 
+    //     return null;
+    // }
+
+
+    public static bool FindMatchingRule(SubjectAttributes subjectAttributes, List<Rule> rules) => rules.FirstOrDefault(rule => MatchesRule(subjectAttributes, rule)) != default;
+
+    private static bool MatchesRule(SubjectAttributes subjectAttributes, Rule rule) => rule.conditions.All(condition => EvaluateCondition(subjectAttributes, condition));
     private static bool EvaluateCondition(SubjectAttributes subjectAttributes, Condition condition)
     {
         try
         {
-            if (subjectAttributes.ContainsKey(condition.attribute) &&
-                subjectAttributes.TryGetValue(condition.attribute, out EppoValue outVal))
+            if (subjectAttributes.TryGetValue(condition.Attribute, out var value))
             {
-                var value = outVal!; // Assuming non-null for simplicity, handle nulls as necessary
-
-                if (condition.operatorType == GTE)
+                switch (condition.Operator)
                 {
-                    if (value.isNumeric() && condition.value.isNumeric())
-                    {
-                        return value.DoubleValue() >= condition.value.DoubleValue();
-                    }
+                    case GTE:
+                        {
+                            if (value.isNumeric() && condition.Value.isNumeric())
+                            {
+                                return value.DoubleValue() >= condition.Value.DoubleValue();
+                            }
 
-                    if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
-                        NuGetVersion.TryParse(condition.value.StringValue(), out var conditionSemver))
-                    {
-                        return valueSemver >= conditionSemver;
-                    }
+                            if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
+                                NuGetVersion.TryParse(condition.Value.StringValue(), out var conditionSemver))
+                            {
+                                return valueSemver >= conditionSemver;
+                            }
 
-                    return false;
-                }
-                else if (condition.operatorType == GT)
-                {
-                    if (value.isNumeric() && condition.value.isNumeric())
-                    {
-                        return value.DoubleValue() > condition.value.DoubleValue();
-                    }
+                            return false;
+                        }
+                    case GT:
+                        {
+                            if (value.isNumeric() && condition.Value.isNumeric())
+                            {
+                                return value.DoubleValue() > condition.Value.DoubleValue();
+                            }
 
-                    if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
-                        NuGetVersion.TryParse(condition.value.StringValue(), out var conditionSemver))
-                    {
-                        return valueSemver > conditionSemver;
-                    }
+                            if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
+                                NuGetVersion.TryParse(condition.Value.StringValue(), out var conditionSemver))
+                            {
+                                return valueSemver > conditionSemver;
+                            }
 
-                    return false;
-                }
-                else if (condition.operatorType == LTE)
-                {
-                    if (value.isNumeric() && condition.value.isNumeric())
-                    {
-                        return value.DoubleValue() <= condition.value.DoubleValue();
-                    }
+                            return false;
+                        }
+                    case LTE:
+                        {
+                            if (value.isNumeric() && condition.Value.isNumeric())
+                            {
+                                return value.DoubleValue() <= condition.Value.DoubleValue();
+                            }
 
-                    if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
-                        NuGetVersion.TryParse(condition.value.StringValue(), out var conditionSemver))
-                    {
-                        return valueSemver <= conditionSemver;
-                    }
+                            if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
+                                NuGetVersion.TryParse(condition.Value.StringValue(), out var conditionSemver))
+                            {
+                                return valueSemver <= conditionSemver;
+                            }
 
-                    return false;
-                }
-                else if (condition.operatorType == LT)
-                {
-                    if (value.isNumeric() && condition.value.isNumeric())
-                    {
-                        return value.DoubleValue() < condition.value.DoubleValue();
-                    }
+                            return false;
+                        }
+                    case LT:
+                        {
+                            if (value.isNumeric() && condition.Value.isNumeric())
+                            {
+                                return value.DoubleValue() < condition.Value.DoubleValue();
+                            }
 
-                    if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
-                        NuGetVersion.TryParse(condition.value.StringValue(), out var conditionSemver))
-                    {
-                        return valueSemver < conditionSemver;
-                    }
+                            if (NuGetVersion.TryParse(value.StringValue(), out var valueSemver) &&
+                                NuGetVersion.TryParse(condition.Value.StringValue(), out var conditionSemver))
+                            {
+                                return valueSemver < conditionSemver;
+                            }
 
-                    return false;
-                }
-                else if (condition.operatorType == MATCHES)
-                {
-                    return Regex.Match(value.StringValue(), condition.value.StringValue(), RegexOptions.IgnoreCase).Success;
-                }
-                else if (condition.operatorType == ONE_OF)
-                {
-                    return Compare.IsOneOf(value.StringValue(), condition.value.ArrayValue());
-                }
-                else if (condition.operatorType == NOT_ONE_OF)
-                {
-                    return !Compare.IsOneOf(value.StringValue(), condition.value.ArrayValue());
+                            return false;
+                        }
+                    case MATCHES:
+                        {
+                            return Regex.Match(value.StringValue(), condition.Value.StringValue(), RegexOptions.IgnoreCase).Success;
+                        }
+                    case ONE_OF:
+                        {
+                            return Compare.IsOneOf(value.StringValue(), condition.Value.ArrayValue());
+                        }
+                    case NOT_ONE_OF:
+                        {
+                            return !Compare.IsOneOf(value.StringValue(), condition.Value.ArrayValue());
+                        }
                 }
             }
-
             return false; // Return false if attribute is not found or other errors occur
         }
         catch (Exception)
